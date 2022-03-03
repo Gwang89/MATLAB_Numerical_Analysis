f3754e37ef4a6b19b89e01172084d61ecf309ac9
GwangSik <99713774+Gwang89@users.noreply.github.com>
2022-03-02 19:47:01 -0800

    Update Showing_Newton.m

--- Showing_Newton.m
+++ Showing_Newton.m
@@ -9,10 +9,8 @@
 
 
 % Course Name  : MATH 2342
-% Team Members : Ashleen Bains
 %                Gwang Sik Kim
-%                Joshua Selikem
-%                Noel Steves
+
 
 % Created Date : Feb 20, 2021
 
